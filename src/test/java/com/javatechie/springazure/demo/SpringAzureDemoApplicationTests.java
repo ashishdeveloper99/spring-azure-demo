58c0c03592fa4cb29bb815616125dd0d75c557af
Ashish <ashishdhomne@hotmail.com>
2021-10-20 23:40:28 +0530

    first commit

---
+++ src/test/java/com/javatechie/springazure/demo/SpringAzureDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.springazure.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAzureDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
